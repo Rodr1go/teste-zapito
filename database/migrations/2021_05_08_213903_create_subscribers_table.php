d16d671dea6ceda294ca0c85d3747992c2de209a
Rodrigo Santos <rodrigosc.pi@gmail.com>
2021-05-09 12:36:38 -0300

    Implementation of subscriber crud

---
+++ database/migrations/2021_05_08_213903_create_subscribers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscribersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('telephone');
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscribers');
+    }
+}
